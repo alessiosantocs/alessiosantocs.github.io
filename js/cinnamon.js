3df9121dc2de4f2318e64021ea34073e4642fec3
Alessio Santo <alessio.santocs@gmail.com>
2015-09-28 22:08:26 +0200

    Update cinnamon.js

--- js/cinnamon.js
+++ js/cinnamon.js
@@ -7,7 +7,6 @@ window.api = {
     $.get("pages/projects/" + method + ".md", callback);
   },
   repositories: function(callback){
-    // $.get("http://api.github.com/users/alessiosantocs/repos?sort=pushed", callback);
     $.ajax({
       url: "http://api.github.com/users/alessiosantocs/repos?sort=pushed",
       crossDomain: true
