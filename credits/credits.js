2511efd8353eb97ff2f89a160e0277a9a3dc71b3
Alessio Santo <alessio.santocs@gmail.com>
2016-05-03 17:23:39 +0200

    Credits system

---
+++ credits/credits.js
@@ -0,0 +1,17 @@
+(function(w, d){
+  var creditContainerTag = d.createElement("div");
+  var creditLinkTag = d.createElement("a");
+  var stylesheet = d.createElement("link");
+  stylesheet.rel ="stylesheet";
+  stylesheet.type="text/css";
+  stylesheet.href="http://aboutalessio.com/credits/credits.css";
+
+  creditContainerTag.id = "as-powercredits-container";
+  creditLinkTag.id = "as-powercredits-a";
+  creditLinkTag.innerHTML = "Alessio Santo";
+  creditContainerTag.appendChild(creditLinkTag);
+
+  var noscripttag = d.findElementById("as-powercredits-container");
+  noscripttag.remove();
+  d.body.appendChild(creditContainerTag);
+})(window, document)
